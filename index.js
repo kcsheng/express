12c44c94cda55ce1e776cda9def4a9fbb1c466d3
kcsheng <kacysheng@mail.com>
2021-06-09 00:13:14 +1000

    Experiment with greq.params.id

--- index.js
+++ index.js
@@ -1,29 +1,30 @@
 const express = require("express");
 const app = express();
-
-// methods available corresponding to http protocol
-// app.get();
-// app.post();
-// app.put();
-// app.delete();
-// with app object, we have no need to set up the server containing heavy route code blocks and their details (setHeader and status code.)
-// The application looks light weight and easy to manage.
+const courses = [
+  { id: 1, name: "course1" },
+  { id: 2, name: "course2" },
+  { id: 3, name: "course3" },
+];
 
 app.get("/", (req, res) => {
   res.send("Hello World");
 });
 
-app.get("/api/names", (req, res) => {
-  res.send(["John", "Mary", "Helen"]);
+app.get("/api/courses", (req, res) => {
+  res.send(courses);
 });
 
-// column will make the query item go into a simple params collection
-app.get("/api/names/:id", (req, res) => {
-  res.send(req.params); // or query string is sent from client side starting with ? such as ?sortBy=name
+app.get("/api/courses/:id", (req, res) => {
+  const course = courses.find(
+    (course) => course.id === parseInt(req.params.id)
+  );
+
+  if (!course) {
+    res.status(404).send("The course with given id not found");
+  }
+  res.send(course);
 });
 
-// Port needs to be dynamic according the host of the app.
-// You could set PORT in terminal using export PORT=4000
 const port = process.env.PORT || 3000;
 app.listen(port, () => {
   console.log(`Listening on port ${port}.`);
